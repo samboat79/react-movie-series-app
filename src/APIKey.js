028d0aff326aca86f5b1f436bd57671652c24787
sammybee04 <sbsamboat9@icloud.com>
2020-08-05 14:41:44 -0500

    commiting movie app

---
+++ src/APIKey.js
@@ -0,0 +1,3 @@
+module.exports = {
+    APIKey: "7536147a"
+};
---
+++ src/components/Text1.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Text1 extends Component {
+    render() {
+        return <span className="fab fa-imdb fa-5x"></span>;
+        
+    }
+}
+
+export default Text1;
